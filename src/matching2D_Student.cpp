70c118a9ed84701036fdabd2deeea66575c3d064
Sai <kakubalsai@gmail.com>
2021-05-17 16:00:39 +0200

    Add AKAZE detector

--- src/matching2D_Student.cpp
+++ src/matching2D_Student.cpp
@@ -229,7 +229,22 @@ void detKeypointsModern(std::vector<cv::KeyPoint> &keypoints, cv::Mat &img, std:
     }
     else if(detectorType.compare("AKAZE") == 0)
     {
-        //...
+        cv::Ptr<cv::FeatureDetector> detector = cv::AKAZE::create();
+
+        double t = (double)cv::getTickCount();
+        detector->detect(img, keypoints);
+        t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
+        cout << "AKAZE detector with n= " << keypoints.size() << " keypoints in " << 1000 * t / 1.0 << " ms" << endl;
+        
+        if (bVis)
+        {
+            cv::Mat visImage = img.clone();
+            cv::drawKeypoints(img, keypoints, visImage, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+            string windowName = "AKAZE Results";
+            cv::namedWindow(windowName, 6);
+            imshow(windowName, visImage);
+            cv::waitKey(0);
+        }
     }
     else if(detectorType.compare("SIFT") == 0)
     {
