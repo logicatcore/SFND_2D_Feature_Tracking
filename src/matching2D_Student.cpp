354ed3c7b82788b46bcc721cffb9d4f8b11b2992
Sai <kakubalsai@gmail.com>
2021-05-17 13:42:24 +0200

    Add FAST detection

--- src/matching2D_Student.cpp
+++ src/matching2D_Student.cpp
@@ -167,3 +167,45 @@ void detKeypointsHarris(std::vector<cv::KeyPoint> &keypoints, cv::Mat &img, bool
         cv::waitKey(0);
     }
 }
+void detKeypointsModern(std::vector<cv::KeyPoint> &keypoints, cv::Mat &img, std::string detectorType, bool bVis)
+{
+    if(detectorType.compare("FAST") == 0)
+    {
+        int threshold = 50;
+        bool NMS = true;
+
+        double t = (double)cv::getTickCount();
+        cv::FAST(img, keypoints, threshold, NMS, cv::FastFeatureDetector::TYPE_9_16);
+        t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
+        cout << "FAST detection with n= " << keypoints.size() << " keypoints in " << 1000 * t / 1.0 << " ms" << endl;
+     
+        if (bVis)
+        {
+            cv::Mat visImage = img.clone();
+            cv::drawKeypoints(img, keypoints, visImage, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+            string windowName = "FAST Results";
+            cv::namedWindow(windowName, 6);
+            imshow(windowName, visImage);
+            cv::waitKey(0);
+        }
+    }
+    else if(detectorType.compare("BRISK") == 0)
+    {
+
+    }
+    else if(detectorType.compare("ORB") == 0)
+    {
+        //...
+    }
+    else if(detectorType.compare("AKAZE") == 0)
+    {
+        //...
+    }
+    else if(detectorType.compare("SIFT") == 0)
+    {
+        //...
+    }
+    else{
+        cout << "#### The chosen detector type is wrong ####" << endl;
+    }
+}
